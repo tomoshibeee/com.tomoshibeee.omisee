9c59f2c0c65031fdb642aab7b6c1d9607f199070
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-04-30 21:27:28 +0900

    fix page crash by handling undefined site before accessing template

--- app/p/[slug]/page.tsx
+++ app/p/[slug]/page.tsx
@@ -1,27 +1,27 @@
-import { sites } from "@/lib/data"
+import { sites } from "@/lib/data";
 
-import Template1 from "@/components/templates/Template1"
-import Template2 from "@/components/templates/Template2"
+import Template1 from "@/components/templates/Template1";
+import Template2 from "@/components/templates/Template2";
 
 export default async function Page({
   params,
 }: {
-  params: Promise<{ slug: string }>
+  params: Promise<{ slug: string }>;
 }) {
-  const { slug } = await params
+  const { slug } = await params;
 
   const templateMap: any = {
     template1: Template1,
-    template2: Template2
-  }
+    template2: Template2,
+  };
 
-  const site = sites.find((s) => s.slug === slug)
+  const site = sites.find((s) => s.slug === slug);
 
-  const Template = templateMap[site.template]
+  if (!site) return <div>Not Found</div>;
 
-  if (!Template) return <div>Template Not Found</div>
+  const Template = templateMap[site.template];
 
-  if (!site) return <div>Not Found</div>
+  if (!Template) return <div>Template Not Found</div>;
 
-  return <Template site={site} />
+  return <Template site={site} />;
 }
