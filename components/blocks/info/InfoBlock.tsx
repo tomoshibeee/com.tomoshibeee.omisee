cd13f89c8b618e728f283dfd89b73b9a42315e2c
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-05-03 19:44:38 +0900

    feat: add props to InfoBlock

--- components/blocks/info/InfoBlock.tsx
+++ components/blocks/info/InfoBlock.tsx
@@ -1,6 +1,10 @@
-export default function InfoBlock({ text }: any) {
+interface Props {
+  text: string;
+}
+
+export default function InfoBlock({ text }: Props) {
   return (
-    <section　className="p-10 grid grid-cols-2 gap-4">
+    <section className="p-10 grid grid-cols-2 gap-4">
       <div className="border p-4">{text}</div>
     </section>
   );
