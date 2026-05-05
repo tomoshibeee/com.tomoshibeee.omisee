7ce65c3a9603830e86aa2f5d7d26066c3e92f03d
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-05-05 19:30:33 +0900

    WIP #11: restructure site config to nest blocks inside sections

--- components/sections/SectionRender.tsx
+++ components/sections/SectionRender.tsx
@@ -2,17 +2,5 @@ import BaseSection from "./BaseSection";
 import { SectionData } from "@/types/section";
 
 export default function SectionRenderer({ section }: { section: SectionData }) {
-  switch (section.type) {
-    case "about":
-      return (
-        <>
-          {section.blocks.map((block: any, i) =>
-            block.type === "about" ? <BaseSection key={i} {...block} /> : null,
-          )}
-        </>
-      );
-
-    default:
-      return null;
-  }
+  return <BaseSection key={i} {...section} />;
 }
