44c7b808ebf991dd2457d4c7c5f090d33473c8a3
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-05-05 19:35:30 +0900

    WIP #11: restructure site config to nest blocks inside sections

--- components/sections/SectionRender.tsx
+++ components/sections/SectionRender.tsx
@@ -2,5 +2,5 @@ import BaseSection from "./BaseSection";
 import { SectionData } from "@/types/section";
 
 export default function SectionRenderer({ section }: { section: SectionData }) {
-  return <BaseSection key={i} {...section} />;
+  return <BaseSection {...section} />;
 }
