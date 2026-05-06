d585a8a517a3760ef4f43caa3e400a3f19e722cb
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-05-06 18:48:20 +0900

    #12 feat: move CTA block above hero section

--- components/blocks/cta/CtaBlock.tsx
+++ components/blocks/cta/CtaBlock.tsx
@@ -1,11 +1,14 @@
 import { CtaData } from "@/types/block";
 
-export default function CtaBlock(data: CtaData) {
+export default function CtaBlock(props: CtaData) {
+  const { href, label } = props;
   return (
-    <section className="p-10 grid grid-cols-2 gap-4">
-      <a href={data.href} className="border p-4 text-center">
-        <div className="border p-4">{data.label}</div>
+    <div className="absolute bottom-10 left-1/2 -translate-x-1/2 z-20">
+      <a href={href} className="p-4 text-center">
+        <div className="border p-4 bg-blue-500 text-white hover:bg-blue-600 rounded-lg transition-colors">
+          {label}
+        </div>
       </a>
-    </section>
+    </div>
   );
 }
