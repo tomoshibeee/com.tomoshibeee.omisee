39c3572b1a72f717c06692593efc6dc5b0d9c633
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-05-08 16:37:43 +0900

    #25 feat: add Google login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,19 @@
-export default function Page() {
+"use client";
+
+import { supabase } from "@/lib/supabase";
+
+export default function LoginPage() {
+  const signInWithGoogle = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+  };
+
   return (
-    <main>
-      <h1>Tomoshibeee Church1 Login Page</h1>
-      <p>it works 🚀</p>
-    </main>
-  )
+    <div className="h-screen flex items-center justify-center">
+      <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+    </div>
+  );
 }
