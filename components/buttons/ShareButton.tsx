a92ea64d76a283447ff3efc64b604058eac12458
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-05-28 09:14:19 +0900

    feat(#42): add share button

--- components/buttons/ShareButton.tsx
+++ components/buttons/ShareButton.tsx
@@ -1,14 +1,31 @@
 import { FaShareFromSquare } from "react-icons/fa6";
 
-export default function ShareButton() {
-  const COLOR = "gray";
+function ShareButton({ className }: { className?: string }) {
   const SIZE = 20;
   return (
     <div
-      className={`flex h-9 w-9 items-center justify-center rounded-full text-${COLOR}-600 transition hover:bg-slate-100 hover:text-${COLOR}-900`}
+      className={className}
     >
       {" "}
       <FaShareFromSquare size={SIZE} />
     </div>
+  );
+}
+
+export function ShareButtonHeader() {
+  const COLOR = "gray";
+  return (
+    <ShareButton
+      className={`flex h-9 w-9 items-center justify-center rounded-full text-${COLOR}-600 transition hover:bg-slate-100 hover:text-${COLOR}-900`}
+    />
+  );
+}
+
+export function ShareButtonFooter() {
+  const COLOR = "gray";
+  return (
+    <ShareButton
+      className={`flex h-9 w-9 items-center justify-center rounded-md bg-white/10 text-${COLOR}-200 transition hover:bg-${COLOR}-600 hover:text-white`}
+    />
   );
 }
