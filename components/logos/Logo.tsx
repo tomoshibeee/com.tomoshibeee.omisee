50b91b030e1ebdf5ce08e97b94afa5908816fc39
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-05-28 14:28:50 +0900

    refactor(#49): separate Header into Logo, DropDownMenu, and HambergerMenu

--- components/logos/Logo.tsx
+++ components/logos/Logo.tsx
@@ -1,12 +1,22 @@
 import { SiteData } from "@/types/site";
 
-export function Logo({site} : {site: SiteData}) {
+export function Logo({ site }: { site: SiteData }) {
   return (
-      <div className="flex items-center gap-2">
-        <span className="text-blue-600 text-base font-semibold">[LG]</span>
-        <span className="text-base font-semibold text-gray-900">
-          {site.meta.name}
-        </span>
+    // TODO : ロゴ画像の表示
+    // {site.meta.logoUrl ? (
+    //   <img src={site.meta.logoUrl} />
+    // ) : (
+    //   <FallbackLogo />
+    // )}
+    <div className="flex items-center gap-2">
+      <div className="flex h-8 w-8 items-center justify-center rounded-md bg-blue-600 text-white text-sm font-bold">
+        {" "}
+        {site.meta.name?.[0] ?? "C"}
       </div>
+
+      <span className="text-base font-semibold text-gray-900">
+        {site.meta.name}
+      </span>
+    </div>
   );
 }
