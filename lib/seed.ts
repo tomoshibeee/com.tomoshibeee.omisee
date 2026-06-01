54f5a45eb8d3dfabe03d26d5165b003c55125af2
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-06-02 07:09:13 +0900

    wip(#67): db migrate → (reset) → seed

--- lib/seed.ts
+++ lib/seed.ts
@@ -1,11 +1,12 @@
 import "dotenv/config";
-import { randomUUID } from "crypto";
 
 import { createClient } from "@supabase/supabase-js";
 
 import { dummyNewsData } from "../data/seed-data-news";
 import { dummySiteData } from "../data/seed-data-sites";
+import { dummySiteMetasData } from "../data/seed-data-site-metas";
 import { dummySiteNewsData } from "../data/seed-data-site-news";
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -46,7 +47,22 @@ async function runSeed() {
   console.log("✅ sites created:", sitesIds);
 
   // =========================
-  // 3. Site News
+  // 3. Site Metas
+  // =========================
+  const { data: siteMetas, error: siteMetasError } = await supabase
+    .from("t_site_metas")
+    .insert(dummySiteMetasData(sitesIds))
+    .select();
+  if (siteMetasError || !siteMetas) {
+    console.error("❌ siteMetas insert error:", siteMetasError);
+    return;
+  }
+
+  const siteMetasIds = sites.map((n) => n.id);
+  console.log("✅ site metas created:", siteMetasIds);
+
+  // =========================
+  // 4. Site News
   // =========================
   const siteNewsRows = dummySiteNewsData(sitesIds);
   const { data: siteNews, error: siteNewsError } =
