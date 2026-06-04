e49dc4523533a4f29068d95c12f19c140dfcd36f
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-06-04 14:21:02 +0900

    wip(#71): replace json data source with db (supabase)

--- data/seed-data-site-metas.ts
+++ data/seed-data-site-metas.ts
@@ -1,53 +1,30 @@
 import { randomUUID } from "crypto";
 import { SiteMeta } from "../models/siteMeta";
 
+import meta1 from "@/lib/data/site1/meta.json";
+import meta2 from "@/lib/data/site2/meta.json";
+import meta3 from "@/lib/data/site3/meta.json";
+
 export function dummySiteMetaModelData(siteIds: string[]) {
     const now = new Date().toISOString();
-
-    const dummySiteMetas = [
-        {
-            id: randomUUID(),
-            site_id: "uuid-site-1",
-            slug: "shizuoka-jonan-church",
-            name: "静岡城南教会",
-            tel: "054-111-1111",
-            email: `info@shizuoka-jonan-church.com`,
-            postal_code: "421-0000",
-            address: `静岡県静岡市葵区城南町${Math.floor(Math.random() * 10) + 1}丁目${Math.floor(Math.random() * 10) + 1}番地`,
-            building: `城南町ビル${Math.floor(Math.random() * 10) + 1}F`,
-            access: `JR静岡駅から徒歩${Math.floor(Math.random() * 10) + 1}分`,
-            created_at: now,
-            updated_at: now,
-        },
-        {
-            id: randomUUID(),
-            site_id: "uuid-site-2",
-            slug: "organic-apple-store",
-            name: "オーガニック・アップル・ストア",
-            tel: "054-222-2222",    
-            email: `info@organic-apple-store.com`,
-            postal_code: "421-0000",
-            address: `静岡県静岡市葵区鷹匠町${Math.floor(Math.random() * 10) + 1}丁目${Math.floor(Math.random() * 10) + 1}番地`,
-            building: `ハイソビル${Math.floor(Math.random() * 10) + 1}F`,
-            access: `JR静岡駅から徒歩${Math.floor(Math.random() * 10) + 1}分`,
-            created_at: now,
-            updated_at: now,
-        },
-        {
-            id: randomUUID(),
-            site_id: "uuid-site-3",
-            slug: "hitoyado-55-megane",
-            name: "人宿55メガネ",
-            tel: "054-333-3333",
-            email: `info@hitoyado-55-megane.com`,
-            postal_code: "421-0000",
-            address: `静岡県静岡市葵区人宿町${Math.floor(Math.random() * 10) + 1}丁目${Math.floor(Math.random() * 10) + 1}番地`,
-            building: `無限相乗ビル${Math.floor(Math.random() * 10) + 1}F`,
-            access: `JR静岡駅から徒歩${Math.floor(Math.random() * 10) + 1}分`,
-            created_at: now,
-            updated_at: now,
-        },
-    ]
+    const metaJson = [meta1, meta2, meta3];
+    const dummySiteMetas =
+        metaJson.map((m, i) => {
+            return {
+                id: randomUUID(),
+                site_id: siteIds[i],
+                slug: m.slug,
+                name: m.name,
+                tel: m.tel,
+                email: m.email,
+                postal_code: m.postalCode,
+                address: m.address,
+                building: m.bldg,
+                access: m.access,
+                created_at: now,
+                updated_at: now,
+            };
+        });
     return siteIds.map((siteId, i) => {
         return {
             ...dummySiteMetas[i % dummySiteMetas.length],
