5a17eacc9e907fbf7c2d5eb594f611d3e910060c
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-06-22 21:19:43 +0900

    wip:(#95) add dashboard

--- components/shared/EditPageContainer.tsx
+++ components/shared/EditPageContainer.tsx
@@ -15,15 +15,36 @@ export default function EditPageContainer(props: Props) {
   const { site } = props;
 
   const [editingBlock, setEditingBlock] = useState<Block | null>(null);
+  const [sections, setSections] = useState(site.layout.sections);
+
   return (
     <>
-      <Template site={site} edit onEdit={(b) => setEditingBlock(b)} />
+      <Template
+        site={{
+          ...site,
+          layout: { ...site.layout, sections: sections },
+        }}
+        edit
+        onEdit={(b) => setEditingBlock(b)}
+      />
 
       {editingBlock && (
         <BlockDrawer
           block={editingBlock}
           open={true}
           onClose={() => setEditingBlock(null)}
+          onChange={(updatedBlock) => {
+            setSections((prev) =>
+              prev.map((section) => ({
+                ...section,
+                blocks: section.blocks.map((b) =>
+                  b.id === updatedBlock.id ? updatedBlock : b,
+                ),
+              })),
+            );
+
+            setEditingBlock(updatedBlock); // ← UI同期（重要）
+          }}
         />
       )}
     </>
--- features/block/BlockDrawer.tsx
+++ features/block/BlockDrawer.tsx
@@ -3,13 +3,17 @@ import { Block } from "@/features/block/index";
 
 import { IoIosCloseCircleOutline } from "react-icons/io";
 
+import { HeroBlockData } from "@/features/block";
+
 type Props = {
   block: Block;
   open: boolean;
   onClose: () => void;
+  onChange: (block: Block) => void;
 };
 
-export function BlockDrawer({ block, open, onClose }: Props) {
+export function BlockDrawer(props: Props) {
+  const { block, open, onClose, onChange } = props;
   const baseDrawerClass =
     "fixed top-0 right-0 h-full w-96 bg-slate-100 shadow-2xl transform transition-transform duration-300 z-50 overflow-y-auto pb-10";
   const drawerClass = open
@@ -21,27 +25,44 @@ export function BlockDrawer({ block, open, onClose }: Props) {
     ? `${baseOverlayClass} transition-opacity duration-300 opacity-100 block`
     : `${baseOverlayClass} transition-opacity duration-300 opacity-0 hidden`;
 
+  let content = null;
+
+  if (block.type === "hero") {
+    const data = block.data as HeroBlockData;
+
+    content = (
+      <input
+        value={data.title ?? ""}
+        onChange={(e) => {
+          const updatedBlock: Block = {
+            ...block,
+            data: {
+              ...data,
+              title: e.target.value,
+            },
+          };
+          onChange(updatedBlock);
+        }}
+      />
+    );
+  } else {
+    content = <div>未実装</div>;
+  }
   return (
     <>
       <div className={overlayClass} onClick={onClose} />
 
       <div className={drawerClass}>
-        <button
-          className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded shadow absolute top-4 right-4 z-50 cursor-pointer"
-          onClick={onClose}
-        >
+        <button onClick={onClose}>
           <IoIosCloseCircleOutline />
         </button>
 
-        <section className="p-6 font-bold text-black max-w-4xl mx-auto">
-          <h2 className="text-xl font-bold mb-6 border-b pb-2 text-gray-800">
-            {`${block.type}-${block.id}`}
+        <section className="p-6">
+          <h2>
+            {block.type}-{block.id}
           </h2>
 
-          <div className="space-y-4">
-            編集画面
-            {JSON.stringify(block.data)}
-          </div>
+          {content}
         </section>
       </div>
     </>
