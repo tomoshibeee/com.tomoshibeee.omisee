26f9f0af55b4b45b02d2941d16665d3a53b54309
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-06-22 21:22:42 +0900

    wip:(#95) add dashboard

--- components/shared/EditPageContainer.tsx
+++ components/shared/EditPageContainer.tsx
@@ -43,7 +43,7 @@ export default function EditPageContainer(props: Props) {
               })),
             );
 
-            setEditingBlock(updatedBlock); // ← UI同期（重要）
+            setEditingBlock(updatedBlock); 
           }}
         />
       )}
