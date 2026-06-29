7e601fae4a2c1948cc94e5c1625423fc3b202018
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-06-30 05:39:28 +0900

    wip(#111): integrate ImageDrawer for Cloudinary image uploads

--- components/media/ImageUploader.tsx
+++ components/media/ImageUploader.tsx
@@ -1,4 +1,5 @@
 import { HeroBlockData } from "@/features/block";
+import { FaCameraRetro } from "react-icons/fa6";
 
 type Props = {
   data: HeroBlockData;
@@ -9,7 +10,7 @@ export default function ImageUploader(props: Props) {
   const { data, onOpenImageUploader } = props;
   const handleEdit = async () => {
     onOpenImageUploader();
-  }
+  };
 
   // const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
   //   const file = e.target.files?.[0];
@@ -23,7 +24,12 @@ export default function ImageUploader(props: Props) {
 
   return (
     <>
-      <button onClick={handleEdit}>📷</button>
+      <button
+        onClick={handleEdit}
+        className="absolute right-4 top-4 z-20 flex items-center justify-center w-10 h-10 rounded-full bg-black/50 text-white hover:bg-black/70"
+      >
+        <FaCameraRetro />
+      </button>
 
       {/*<input
         ref={inputRef}
